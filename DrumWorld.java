4e3778c7207294a909cb5165d60aca71a2cef59a
Benjamin Blankholm <snurresvup@hotmail.com>
2017-01-03 11:33:59 +0100

    initial commit

---
+++ DrumWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class DrumWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DrumWorld extends World
+{
+
+    /**
+     * Constructor for objects of class DrumWorld.
+     * 
+     */
+    public DrumWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(750, 572, 1); 
+    }
+}
